5d2e80c590a45bef8681d7d10a11b99128840dd2
Logan Kulinski <rashes_lineage02@icloud.com>
2024-10-24 12:48:18 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# league-youtube-uploader
+This project houses a simple Spring `ApplicationRunner` that will read the video files out of the specified path and 
+upload them to a specified YouTube channel.
+
+## Gaining access to the YouTube API
+1. Create a Google Cloud project and enable the YouTube Data API v3
+   1. You can follow the guide [here](https://developers.google.com/youtube/v3/quickstart/java) for how to do so. It is
+free
+2. Create OAuth 2.0 credentials and download the `client_secret_CLIENTID.json`, where `CLIENTID` is the client ID for
+your project
+3. Rename the downloaded file to `client_secret.json`
+
+## Running the application
+1. Make sure you have Java 23 or higher installed
+    1. If needed, you can download it from [jdk.java.net](https://jdk.java.net/23/)
+2. Clone the repository
+3. Navigate to the project directory
+4. Place the `client_secret.json` file in `src/main/resources`
+5. Ensure that the `VIDEOS_PATH` environment variable is set to the path where your video files are located
+6. Build the project using Maven:
+    ```bash
+    ./mvnw clean install
+    ```
+7. Run the application:
+    ```bash
+    ./mvnw spring-boot:run
+    ```
+8. Follow the prompts to authenticate with your Google account and authorize the application to access your YouTube
+9. channel
+10. The application will then read the video files from the specified path and upload them to your YouTube channel
+
---
+++ src/main/java/net/lbku/VideoUploadRunner.java
@@ -0,0 +1,219 @@
+package net.lbku;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.InputStreamContent;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.Video;
+import com.google.api.services.youtube.model.VideoSnippet;
+import com.google.api.services.youtube.model.VideoStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.GeneralSecurityException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+@Component
+public final class VideoUploadRunner implements ApplicationRunner {
+    private final String videosPath;
+
+    private final GsonFactory gsonFactory;
+
+    private static final Logger LOGGER;
+
+    static {
+        LOGGER = LoggerFactory.getLogger(VideoUploadRunner.class);
+    }
+
+    @Autowired
+    public VideoUploadRunner(@Value("${videos.path}") String videosPath) {
+        this.videosPath = Objects.requireNonNull(videosPath);
+
+        this.gsonFactory = GsonFactory.getDefaultInstance();
+    }
+
+    private Map<LocalDate, List<File>> getDatesToVideos() {
+        Path videosDirectory = Path.of(this.videosPath);
+
+        Map<LocalDate, List<File>> datesToVideos = new TreeMap<>();
+
+        try (var stream = Files.newDirectoryStream(videosDirectory)) {
+            for (Path path : stream) {
+                File file = path.toFile();
+
+                if (file.isDirectory()) {
+                    continue;
+                }
+
+                String name = file.getName();
+
+                String[] parts = name.split(" ");
+
+                if (parts.length < 2) {
+                    VideoUploadRunner.LOGGER.warn("File \"{}\" does not start with a date", name);
+
+                    continue;
+                }
+
+                String dateString = parts[0];
+
+                LocalDate date = LocalDate.parse(dateString);
+
+                datesToVideos.compute(date, (_, value) -> {
+                    if (value == null) {
+                        value = new ArrayList<>();
+                    }
+
+                    value.add(file);
+
+                    return value;
+                });
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return datesToVideos;
+    }
+
+    private Credential getCredential(NetHttpTransport httpTransport) throws IOException {
+        Path path = Path.of("src/main/resources/client_secret.json");
+
+        InputStream inputStream = Files.newInputStream(path);
+
+        List<String> scopes = List.of(
+            "https://www.googleapis.com/auth/youtube.upload",
+            "https://www.googleapis.com/auth/youtube",
+            "https://www.googleapis.com/auth/youtubepartner",
+            "https://www.googleapis.com/auth/youtube.force-ssl"
+        );
+
+        try (InputStreamReader inputStreamReader = new InputStreamReader(inputStream)) {
+            GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(this.gsonFactory, inputStreamReader);
+
+            GoogleAuthorizationCodeFlow.Builder builder = new GoogleAuthorizationCodeFlow.Builder(httpTransport,
+                this.gsonFactory, clientSecrets, scopes);
+
+            GoogleAuthorizationCodeFlow flow = builder.build();
+
+            LocalServerReceiver receiver = new LocalServerReceiver();
+
+            AuthorizationCodeInstalledApp app = new AuthorizationCodeInstalledApp(flow, receiver);
+
+            return app.authorize("user");
+        }
+    }
+
+    private YouTube getYouTube() throws GeneralSecurityException, IOException {
+        NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+
+        Credential credential = this.getCredential(httpTransport);
+
+        YouTube.Builder builder = new YouTube.Builder(httpTransport, this.gsonFactory, credential);
+
+        builder.setApplicationName("YouTube League Uploader");
+
+        return builder.build();
+    }
+
+    private Video getVideo(LocalDate date, int index, int count) {
+        Video video = new Video();
+
+        VideoSnippet snippet = new VideoSnippet();
+
+        snippet.setCategoryId("20");
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+
+        String dateString = formatter.format(date);
+
+        String title = String.format("LoL Season 2024 - Split 3 -- %s %d/%d", dateString, index, count);
+
+        VideoUploadRunner.LOGGER.info("Uploading {}...", title);
+
+        snippet.setTitle(title);
+
+        video.setSnippet(snippet);
+
+        VideoStatus status = new VideoStatus();
+
+        status.setPrivacyStatus("unlisted");
+
+        video.setStatus(status);
+
+        return video;
+    }
+
+    private void uploadVideo(YouTube youTube, File file, LocalDate date, int index, int count) {
+        Objects.requireNonNull(youTube);
+
+        Objects.requireNonNull(file);
+
+        Video video = this.getVideo(date, index, count);
+
+        try (FileInputStream fileInputStream = new FileInputStream(file);
+             BufferedInputStream inputStream = new BufferedInputStream(fileInputStream)) {
+            String type = "application/octet-stream";
+
+            InputStreamContent mediaContent = new InputStreamContent(type, inputStream);
+
+            YouTube.Videos.Insert request = youTube.videos()
+                                                   .insert(List.of("snippet", "status"), video, mediaContent);
+
+            Video response = request.execute();
+
+            VideoUploadRunner.LOGGER.info("Upload response: {}", response);
+        } catch (IOException e) {
+            String message = e.getMessage();
+
+            VideoUploadRunner.LOGGER.error(message, e);
+
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void run(ApplicationArguments args) {
+        YouTube youTube;
+
+        try {
+            youTube = this.getYouTube();
+        } catch (GeneralSecurityException | IOException e) {
+            String message = e.getMessage();
+
+            VideoUploadRunner.LOGGER.error(message, e);
+
+            throw new RuntimeException(e);
+        }
+
+        Map<LocalDate, List<File>> datesToVideos = this.getDatesToVideos();
+
+        datesToVideos.forEach((date, files) -> {
+            for (int i = 0; i < files.size(); i++) {
+                File file = files.get(i);
+
+                int index = i + 1;
+
+                int count = files.size();
+
+                this.uploadVideo(youTube, file, date, index, count);
+            }
+        });
+    }
+}
